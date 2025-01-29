5a90bdbc08287fc5b792d7ec4d1e190de27e8d14
Romain <romain.sales31@gmail.com>
2025-01-29 15:08:40 +0100

    implemented plantpage in router

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NavBar } from '@molecules/NavBar';
 import "./styles/style.css"
+import { PlantPage } from '@pages/PlantPage';
 
 const queryClient = new QueryClient();
 
@@ -28,7 +29,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/settings" element={<p>paramètres de l'app</p>} />
 
           {/* Route pour la page de détails d'une plante */}
-          <Route path="/plant/:id" element={<p>consulter détails plante</p>} />
+          <Route path="/plant/:id" element={<PlantPage />} />
 
           {/* Route pour la page de détails d'une utilisateurs */}
           <Route path="/user/:id" element={<p>consulter détails utilisateur</p>} />
