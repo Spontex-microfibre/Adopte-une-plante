da27ff9061c70955794ae5bd948ca9f8dd6299dd
Romain <romain.sales31@gmail.com>
2025-01-29 16:34:19 +0100

    added new queries

--- src/store/queries.ts
+++ src/store/queries.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { getPlants } from "@api/plants";
+import { getPlantById, getPlants, getPlantsByUserId } from "@api/plants";
+import { getUserById } from "@api/users";
 
 export function usePlants() {
     return useQuery({
@@ -9,4 +10,34 @@ export function usePlants() {
         refetchOnWindowFocus: false, // Empêche le refetch automatique quand on revient sur la page
         refetchOnMount: false, // Empêche un refetch automatique au montage si les données existent déjà
     })
+}
+
+export function useUserById(userId: number) {
+    return useQuery({
+        queryFn: () => getUserById(userId), // Fonction de fetch pour récupérer l'utilisateur par ID
+        queryKey: ["userById", userId], // Utilise l'ID de l'utilisateur pour la clef de cache
+        staleTime: 1000 * 60 * 5, // Garde les données valides pendant 5 minutes
+        refetchOnWindowFocus: false, // Empêche le refetch automatique quand on revient sur la page
+        refetchOnMount: false, // Empêche un refetch automatique au montage si les données existent déjà
+    });
+}
+
+export function usePlantById(plantId: number) {
+    return useQuery({
+        queryFn: () => getPlantById(plantId), // Fonction de fetch pour récupérer l'utilisateur par ID
+        queryKey: ["plantById", plantId], // Utilise l'ID de l'utilisateur pour la clef de cache
+        staleTime: 1000 * 60 * 5, // Garde les données valides pendant 5 minutes
+        refetchOnWindowFocus: false, // Empêche le refetch automatique quand on revient sur la page
+        refetchOnMount: false, // Empêche un refetch automatique au montage si les données existent déjà
+    });
+}
+
+export function usePlantsByUserId(userId: number) {
+    return useQuery({
+        queryFn: () => getPlantsByUserId(userId), // Fonction de fetch pour récupérer l'utilisateur par ID
+        queryKey: ["plantByUserId", userId], // Utilise l'ID de l'utilisateur pour la clef de cache
+        staleTime: 1000 * 60 * 5, // Garde les données valides pendant 5 minutes
+        refetchOnWindowFocus: false, // Empêche le refetch automatique quand on revient sur la page
+        refetchOnMount: false, // Empêche un refetch automatique au montage si les données existent déjà
+    });
 }
