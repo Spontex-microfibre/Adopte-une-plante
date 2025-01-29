265dd8f48dc2b0d30fb20a66df9dfa05015f31d1
Romain <romain.sales31@gmail.com>
2025-01-29 12:59:42 +0100

    removed api call with all users

--- src/api/users.ts
+++ src/api/users.ts
@@ -3,11 +3,6 @@ import { IUser } from "types/user";
 
 const API_URL = "https://6799e2df747b09cdccccdd02.mockapi.io/api/"
 
-export const getUsers = async () => {
-    const response = await axios.get<IUser>(`${API_URL}users`)
-    return response.data;
-}
-
 export const getUserById = async (id: number) => {
     const response = await axios.get<IUser>(`${API_URL}users/${id}`)
     return response.data;
