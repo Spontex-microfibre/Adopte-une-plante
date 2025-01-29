1fd308c2f81188f5327bb6a53757413d7d4d7a50
Romain <romain.sales31@gmail.com>
2025-01-29 11:42:30 +0100

    added users query

--- src/store/queries.ts
+++ src/store/queries.ts
@@ -1,9 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getPlants } from "@api/plants";
+import { getUsers } from "@api/users";
 
 export function usePlants() {
     return useQuery({
         queryFn: getPlants, // fonction de fetch à executer pour la query
         queryKey: ["plants"] // on attribue un identifiant UNIQUE à cette req pour l'invalider + tard si besoin
+    })
+}
+
+export function useUsers() {
+    return useQuery({
+        queryFn: getUsers, // fonction de fetch à executer pour la query
+        queryKey: ["users"] // on attribue un identifiant UNIQUE à cette req pour l'invalider + tard si besoin
     })
 }
