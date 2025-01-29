9fc9ef30ba7290e57bd5108acd2105b2d40f92df
Romain <romain.sales31@gmail.com>
2025-01-29 10:32:48 +0100

    added ts interfaces

---
+++ src/types/plant.ts
@@ -0,0 +1,9 @@
+export interface plant {
+    id: number
+    type: string
+    name: string
+    description: string
+    image: string
+    userId: number
+    availableForAdoption: boolean
+}
---
+++ src/types/user.ts
@@ -0,0 +1,8 @@
+export interface user {
+    firstname: string
+    lastname: string
+    age: number
+    avatar: string
+    location: string
+    id: number
+}
