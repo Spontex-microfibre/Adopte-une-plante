356c840ced7a22652da77471b04a8d5188c8d6c2
Romain <romain.sales31@gmail.com>
2025-01-29 10:46:44 +0100

    created basic routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    salut
+    <Router>
+      <Routes>
+        {/* Route principale */}
+        <Route path="/" element={<p>home</p>} />
+
+        {/* Route pour la page de détails d'une plante */}
+        <Route path="/plant/:id" element={<p>consulter détails plante</p>} />
+
+        {/* Route pour la page de détails d'une utilisateurs */}
+        <Route path="/user/:id" element={<p>consulter détails utilisateur</p>} />
+
+        {/* Route par défaut */}
+        <Route path="*" element={<p>home</p>} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
