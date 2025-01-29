495cd25cf84d3eda4e474dbb7f4da39a18d11975
Romain <romain.sales31@gmail.com>
2025-01-29 13:00:16 +0100

    temp first view

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,33 @@
+import { Home } from '@pages/Home';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { NavBar } from '@molecules/NavBar';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        {/* Route principale */}
-        <Route path="/" element={<p>home</p>} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <NavBar />
+        <Routes>
+          {/* Route principale */}
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<p>consulter mon profil</p>} />
+          <Route path="/settings" element={<p>paramètres de l'app</p>} />
 
-        {/* Route pour la page de détails d'une plante */}
-        <Route path="/plant/:id" element={<p>consulter détails plante</p>} />
+          {/* Route pour la page de détails d'une plante */}
+          <Route path="/plant/:id" element={<p>consulter détails plante</p>} />
 
-        {/* Route pour la page de détails d'une utilisateurs */}
-        <Route path="/user/:id" element={<p>consulter détails utilisateur</p>} />
+          {/* Route pour la page de détails d'une utilisateurs */}
+          <Route path="/user/:id" element={<p>consulter détails utilisateur</p>} />
 
-        {/* Route par défaut */}
-        <Route path="*" element={<p>home</p>} />
-      </Routes>
-    </Router>
+          {/* Route par défaut */}
+          <Route path="*" element={<p>home</p>} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   </StrictMode>,
 )
