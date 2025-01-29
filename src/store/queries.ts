8e8312d1c9ebc5b486a629b1b3586cfc48bf2ba7
Romain <romain.sales31@gmail.com>
2025-01-29 14:32:18 +0100

    fixed refetching on route change

--- src/store/queries.ts
+++ src/store/queries.ts
@@ -4,6 +4,9 @@ import { getPlants } from "@api/plants";
 export function usePlants() {
     return useQuery({
         queryFn: getPlants, // fonction de fetch à executer pour la query
-        queryKey: ["plants"] // on attribue un identifiant UNIQUE à cette req pour l'invalider + tard si besoin
+        queryKey: ["plants"], // on attribue un identifiant UNIQUE à cette req pour l'invalider + tard si besoin
+        staleTime: 1000 * 60 * 5, // Garde les données valides pendant 5 minutes
+        refetchOnWindowFocus: false, // Empêche le refetch automatique quand on revient sur la page
+        refetchOnMount: false, // Empêche un refetch automatique au montage si les données existent déjà
     })
 }
