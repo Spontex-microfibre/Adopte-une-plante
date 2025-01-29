34f755c9556134028a768c5a723bc6e3c3691001
Romain <romain.sales31@gmail.com>
2025-01-29 13:46:56 +0100

    pages organisation fix

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+export const Home: React.FC = () => {
+
+    return (
+        <main className="page-layout">
+            <h1>Bienvenue sur "Adopte une Plante" !</h1>
+            <p>
+                Vous en avez assez des plantes qui ne reçoivent pas assez d'amour ?
+                "Adopte une Plante" est une application qui vous permet d'adopter des
+                plantes en quête d'un foyer aimant. Trouvez la plante qui vous correspond
+                et offrez-lui un nouvel espace pour grandir.
+            </p>
+
+            <section>
+                <h2>Notre mission</h2>
+                <p>
+                    L'objectif de notre application est de rendre les plantes accessibles
+                    à tous ceux qui souhaitent ajouter une touche de verdure à leur vie.
+                    Que vous soyez un expert en jardinage ou un simple amateur, nous avons
+                    la plante parfaite pour vous !
+                </p>
+
+                <div>
+                    <Link to="/plants">
+                        Voir toutes les plantes disponibles
+                    </Link>
+                </div>
+            </section>
+
+        </main>
+    )
+}
